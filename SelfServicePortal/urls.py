0afcbb406e16af73023179fba0b35066948f7dcd
zero19 <hybhoyar@gmail.com>
2015-04-16 20:13:01 +0530

    Added frontend .. urls and redirects working fine

--- SelfServicePortal/urls.py
+++ SelfServicePortal/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     url(r'^login/$', 'signups.views.login_view', name='Login'),
     url(r'^logout/$', 'signups.views.logout_view', name='Logout'),
     url(r'^thankyou/$', 'signups.views.thankyou', name='Thank You'),
+    url(r'^createvm/$', 'userdash.views.createInstance', name='User Dash'),
+    url(r'^viewvms/$', 'userdash.views.viewInstance', name='User Dash'),
+    url(r'^usage/$', 'userdash.views.usageHistory', name='User Dash'),
+
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
